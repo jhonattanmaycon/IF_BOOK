31798a22ea3b198dd8747d559afd33ab0c364590
Caio Anderson <caioandersonmartins@gmail.com>
2021-03-14 22:42:13 -0300

    Aplicação do Modal

--- resources/views/templates/feed.blade.php
+++ resources/views/templates/feed.blade.php
@@ -7,9 +7,11 @@
 
 
 <section class="d-flex flex-column justify-content-center align-items-center">
-	<a href="{{ route('posts.create') }}"> <button class="btn add-post"><svg xmlns="http://www.w3.org/2000/svg" width="50"  fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
-</svg></button></a>
+
+      <!-- Botão que irá abrir o modal -->
+    <button type="button" class="btn btn-success btn-lg mt-2 ml-2" data-toggle="modal" data-target="#meuModal">Fazer publicação</button>
+
+
 </section>
 
 
